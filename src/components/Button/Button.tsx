30ab1b5ba84379e8f1a2dd2f55edf6f553c42db7
Kamil Pyszkowski <kamilpyszkowski@gmail.com>
2023-01-30 23:07:11 +0100

    Cleaned up repo

--- src/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Button(props: React.PropsWithChildren & { foo?: string }) {
-  return <button data-bar={props.foo}>{props.children}</button>;
-}
-
-export default Button;
